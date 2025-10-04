c51ae049fcba3d342c296e8919bb089cf06ed7cd
Татьяна <@DESKTOP-EV5CRI3>
2025-10-04 18:12:11 +0300

    Добавьте файлы проекта.

---
+++ MergeSort/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 5, 2, 9, 1, 3, 7, 6, 8, 4, 0 };
+        Console.WriteLine("Исходный массив: " + string.Join(", ", array));
+
+        MergeSort(array, 0, array.Length - 1);
+
+        Console.WriteLine("Отсортированный массив: " + string.Join(", ", array));
+    }
+
+    static void MergeSort(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+            MergeSort(arr, left, middle);
+            MergeSort(arr, middle + 1, right);
+            Merge(arr, left, middle, right);
+        }
+    }
+
+    static void Merge(int[] arr, int left, int middle, int right)
+    {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[middle + 1 + j];
+
+        int iIndex = 0, jIndex = 0, k = left;
+
+        while (iIndex < n1 && jIndex < n2)
+        {
+            if (L[iIndex] <= R[jIndex])
+            {
+                arr[k] = L[iIndex];
+                iIndex++;
+            }
+            else
+            {
+                arr[k] = R[jIndex];
+                jIndex++;
+            }
+            k++;
+        }
+
+        while (iIndex < n1)
+        {
+            arr[k] = L[iIndex];
+            iIndex++;
+            k++;
+        }
+
+        while (jIndex < n2)
+        {
+            arr[k] = R[jIndex];
+            jIndex++;
+            k++;
+        }
+    }
+}
